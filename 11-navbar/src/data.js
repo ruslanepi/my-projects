13e5e28cbaa05de6a7ae34c898aec0bb18ff0a94
ruslanepi <ruslanep2@gmail.com>
2021-08-24 00:25:20 +0300

    added navbar example

---
+++ 11-navbar/src/data.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'random',
+  },
+  {
+    id: 2,
+    url: '/about',
+    text: 'about',
+  },
+  {
+    id: 3,
+    url: '/projects',
+    text: 'projects',
+  },
+  {
+    id: 4,
+    url: '/contact',
+    text: 'contact',
+  },
+  {
+    id: 5,
+    url: '/profile',
+    text: 'profile',
+  },
+]
+
+export const social = [
+  {
+    id: 1,
+    url: 'https://www.twitter.com',
+    icon: 'fb',
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+    icon: 'tw',
+  },
+  {
+    id: 3,
+    url: 'https://www.twitter.com',
+    icon: 'ln',
+  },
+  {
+    id: 4,
+    url: 'https://www.twitter.com',
+    icon: 'bh',
+  },
+]
