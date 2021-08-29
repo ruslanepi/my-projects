dfd3058e6f4eb412c1d59e7aa02e52591cdf7019
ruslanepi <ruslanep2@gmail.com>
2021-08-29 16:35:38 +0300

    created cocktails-project

--- 15-cocktails/src/components/CocktailList.js
+++ 15-cocktails/src/components/CocktailList.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import Cocktail from './Cocktail'
 import Loading from './Loading'
+
 import { useGlobalContext } from '../context'
 
 function CocktailList() {
+  const { cocktails, loading } = useGlobalContext()
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (cocktails.length < 1) {
+    return <h2 className='section-title'>no cocktails</h2>
+  }
   return (
-    <div>
-      <h2>CocktailList</h2>
-    </div>
+    <section className='section'>
+      <h2 className='section-title'>cocktails</h2>
+      <div className='cocktails-center'>
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />
+        })}
+      </div>
+    </section>
   )
 }
 
